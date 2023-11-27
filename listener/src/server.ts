ff2db7b4a21c0f2370f015529e8db497a97f0b9b
MTthoas <matthias.pecquery@gmail.com>
2023-11-27 23:33:22 +0100

    listener done

--- listener/src/server.ts
+++ listener/src/server.ts
@@ -6,10 +6,12 @@ const app = express();
 const port = 5111; 
 
 app.use(bodyParser.json());
-const repo = '';
+const repo = 'mtthoas/projet-devops:develop';
+const containerName = 'listener';
 
 
 function pullImage() {
+    
     exec(`docker pull ${repo}`, (err: any, stdout: any, stderr: any) => {
         if(err) {
             return;
@@ -18,11 +20,44 @@ function pullImage() {
     });
 }
 
+function startContainer(containerName: string, image: string) {
+    exec(`docker run -d --name ${containerName} ${image}`, (runErr: any, runStdout: any, runStderr: any) => {
+        if (runErr) {
+            console.error(`Erreur lors du démarrage du conteneur ${containerName}: ${runErr}`);
+            return;
+        }
+        console.log(`Conteneur ${containerName} démarré avec l'image ${image}`);
+    });
+}
+
+
+function stopAndRemoveContainer(containerName: string) {
+
+    exec(`docker stop ${containerName}`, (stopErr: any, stopStdout: any, stopStderr: any) => {
+        if (stopErr) {
+            console.error(`Erreur lors de l'arrêt du conteneur ${containerName}: ${stopErr}`);
+            return;
+        }
+        console.log(`Conteneur ${containerName} arrêté`);
+
+        exec(`docker rm ${containerName}`, (rmErr: any, rmStdout: any, rmStderr: any) => {
+            if (rmErr) {
+                console.error(`Erreur lors de la suppression du conteneur ${containerName}: ${rmErr}`);
+                return;
+            }
+            console.log(`Conteneur ${containerName} supprimé`);
+        });
+    });
+}
+
+
 // Pull de l'image toutes les 25 secondes, utile quand on a une adresse privée
 
 setInterval(() => {
-    console.log('Still here');
-    pullImage()
+    console.log("Listening...")
+    stopAndRemoveContainer(containerName);
+    pullImage();
+    startContainer(containerName, repo);
     
 }, 30000);
 
